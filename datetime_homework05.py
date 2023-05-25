518742db69f1a09e3d4c76459d3319e34120e133
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-25 22:26:17 +0500

    5

--- datetime_homework05.py
+++ datetime_homework05.py
@@ -1,2 +1,26 @@
 #Task 5:
 #Write a Python program that prompts the user to enter a specific date in the format "dd-mm-yyyy" and determines the day of the week on which that date falls.
+import datetime
+a=int(input('kunni kiriting '))
+b=int(input('oyni kiriting '))
+c=int(input('yilni kiriting '))
+data = datetime.date.today()
+f1=abs(a-data.day)
+f2=abs(b-data.month)*30
+f3=abs(c-data.year)*365
+
+farq=f1+f2+f3
+if farq%7==3:
+    print('Monday')
+if farq%7==4:
+    print("Tuesday")
+if farq%7==5:
+    print("Wednesday")
+if farq%7==6:
+    print('Thursday')
+if farq%7==0:
+    print("Friday")
+if farq%7==1:
+    print("Satuday ")
+if farq%7==2:
+    print("Sunday")
