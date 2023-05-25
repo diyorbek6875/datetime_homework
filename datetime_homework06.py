57cd75669082395bdf5462418eb0a10c63e6f8f0
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-25 22:38:22 +0500

    6

--- datetime_homework06.py
+++ datetime_homework06.py
@@ -1,2 +1,13 @@
 #Task 6:
 #Write a Python program that accepts a date in the format "dd-mm-yyyy" and checks if it is a leap year. Display an appropriate message indicating whether it is a leap year or not.
+import datetime
+
+c=int(input('yilni kiriting '))
+data = datetime.date.today()
+
+f3=abs(c-data.year)
+if f3%4==3:
+    print("Maskur yil kabisa yili ")
+else :
+    print("Bu yil kabisa yili emas ")
+
