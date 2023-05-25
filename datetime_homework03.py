11529cbea7eb91c64db4f5bd4a85a56e2ba2a3da
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-25 20:33:39 +0500

    3

--- datetime_homework03.py
+++ datetime_homework03.py
@@ -1,2 +1,16 @@
 #Task 3:
 #Write a Python program that prompts the user to enter a specific date in the format "dd-mm-yyyy" and calculates the number of days between the current date and the entered date.
+import datetime
+a=int(input('kunni kiriting '))
+b=int(input('oyni kiriting '))
+c=int(input('yilni kiriting '))
+data = datetime.date.today()
+#if data.day>=a:
+ #   a1=data.day-a
+#else :
+#    a1=a-data.day
+#if data.month>
+f1=abs(a-data.day)
+f2=abs(b-data.month)*30
+f3=abs(c-data.year)*360
+print(f1+f2+f3)
