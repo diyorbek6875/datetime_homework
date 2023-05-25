98410b76def67c27c8f31b7cd24e868c307111b8
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-25 21:55:11 +0500

    4

--- datetime_homework04.py
+++ datetime_homework04.py
@@ -1,2 +1,7 @@
 #Task 4:
 #Write a Python program that calculates the age of a person based on their birth year. Prompt the user to enter their birth year, and display their current age.
+import datetime
+a=int(input("Tug'ilgan yilingizni kiriting "))
+data=datetime.date.today()
+yoshi= data.year-a
+print(yoshi)
