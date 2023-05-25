f8d1ba3496c7285ebb68cc64cddd1fbd4cb1b4fa
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-25 22:51:00 +0500

    8

--- datetime_homework08.py
+++ datetime_homework08.py
@@ -1,2 +1,5 @@
 #Task 8:
 #Write a Python program that subtracts 10 hours from the current time and displays the resulting time.
+import datetime
+data = datetime.datetime.today()
+print(data.hour-10, data.minute)
